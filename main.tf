8eae30cf54739717ab0cd03135a26428a18f2307
Nicat Isa <nicat.isayev@hotmail.com>
2023-06-29 15:30:14 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,42 @@
+### IAM POLICY FOR CODEBUILD
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild_role"
+  assume_role_policy = jsonencode({
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        }
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs_policy" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_policy" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_policy" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+### CREATE RESOURCES
+
 resource "aws_ecr_repository" "repository" {
   name = "react-image-compressor"
 }
@@ -5,7 +44,7 @@ resource "aws_ecr_repository" "repository" {
 resource "aws_codebuild_project" "project" {
   name           = "react-image-compressor"
   description    = "CodeBuild project for react-image-compressor"
-  service_role   = "aws_iam_role.codebuild_role.arn"
+  service_role   = aws_iam_role.codebuild_role.arn
   build_timeout  = "5"
   queued_timeout = "5"
 
@@ -22,6 +61,7 @@ resource "aws_codebuild_project" "project" {
 
   source {
     type     = "GITHUB"
-    location = "https://github.com/Rahul-Pandey7/react-image-compressor"
+    location = "https://github.com/NicatIsa/react-image-compressor"
   }
+
 }
